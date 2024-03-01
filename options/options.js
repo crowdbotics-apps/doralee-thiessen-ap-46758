14217898a0db83028315d288dc5ff4460d6af373
Crowdbotics <team@crowdbotics.com>
2024-03-01 19:50:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "doralee-thiessen-ap-46758",
+  url: "https://doralee-thiessen-ap-46758.botics.co",
+  api: "https://doralee-thiessen-ap-46758.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
